025a80beb2002fe2ed02db565131c5f4ea4fde33
leitao1 <2534545316@qq.com>
2020-05-11 11:36:59 +0800

    train time

---
+++ base_study/train_time.py
@@ -0,0 +1,8 @@
+import time
+import datetime
+
+print(time.time())
+print(time.localtime())
+print(time.mktime((2018, 9, 30, 9, 44, 31, 6, 273, 0)))
+print(datetime.time(9, 44, 31))
+print(datetime.datetime(2018, 9, 30,9, 44, 31))
