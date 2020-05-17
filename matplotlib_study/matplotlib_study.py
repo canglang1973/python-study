0ef71936c0cd9b10348a3fd8ef2c7b7eef4942b7
leitao1 <2534545316@qq.com>
2020-05-17 20:09:01 +0800

    add matplotlib train

---
+++ matplotlib_study/matplotlib_study.py
@@ -0,0 +1,12 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: leitao
+@time: 2020-5-17 20:05
+@desc:
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.arange(10)
+plt.plot(data)
